d01313a3a097f510641ad36fbc889f6926ac562a
lee-man <minli.amoy@gmail.com>
2021-01-15 13:32:15 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,33 @@
 cd src
+# PyMiniSolvers
 git clone https://github.com/liffiton/PyMiniSolvers.git
 cd PyMiniSolvers
 make
-cd ../..
+cd ../
+# abc
+git clone https://github.com/berkeley-abc/abc.git
+cd abc
+make
+make libabc.a
+cd ../
+# Aiger
+mkdir aiger
+cd aiger
+wget http://fmv.jku.at/aiger/aiger-1.9.9.tar.gz
+tar -xf aiger-1.9.9.tar.gz
+mv aiger-1.9.9.tar.gz aiger
+rm aiger-1.9.9.tar.gz
+cd aiger
+./configure.sh && make
+cd ../
+# cnf2aig
+wget http://fmv.jku.at/cnf2aig/cnf2aig-1.tar.gz
+tar -xf cnf2aig-1.tar.gz
+rm cnf2aig-1.tar.gz
+cd cnf2aig
+./configure && make
+
+cd ../../..
+
 
 mkdir data model log
