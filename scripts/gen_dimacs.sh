199c448baaffae48256be0076ec91ac25cfc5bb3
lee-man <minli.amoy@gmail.com>
2021-01-16 11:08:11 +0800

    I am not familiar with shell script...

--- scripts/gen_dimacs.sh
+++ scripts/gen_dimacs.sh
@@ -6,16 +6,16 @@
 # done;
 
 # train
-DIR_TRAIN = "data/dimacs/train/sr3to10"
-DIR_LOG = "log/gen_sr3to10_train.log"
+DIR_TRAIN="data/dimacs/train/sr3to10"
+DIR_LOG="log/gen_sr3to10_train.log"
 rm -rf ${DIR_TRAIN}
 mkdir -p ${DIR_TRAIN}
 
 python src/gen_sr_dimacs.py ${DIR_TRAIN} ${DIR_LOG} 100 --min_n 3 --max_n 10
 
 # validation
-DIR_VAL = "data/dimacs/validation/sr10"
-DIR_LOG = "log/gen_sr10_validation.log"
+DIR_VAL="data/dimacs/validation/sr10"
+DIR_LOG="log/gen_sr10_validation.log"
 rm -rf ${DIR_VAL}
 mkdir -p ${DIR_VAL}
 
