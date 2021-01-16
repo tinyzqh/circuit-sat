798d10f4765f168abeb76a8550805fbbc359d756
lee-man <minli.amoy@gmail.com>
2021-01-16 11:06:51 +0800

    fix bugs.

--- scripts/gen_dimacs.sh
+++ scripts/gen_dimacs.sh
@@ -8,17 +8,17 @@
 # train
 DIR_TRAIN = "data/dimacs/train/sr3to10"
 DIR_LOG = "log/gen_sr3to10_train.log"
-rm -rf $DIR_TRAIN
-mkdir -p $DIR_TRAIN
+rm -rf ${DIR_TRAIN}
+mkdir -p ${DIR_TRAIN}
 
-python src/gen_sr_dimacs.py $DIR_TRAIN $DIRLOG 100 --min_n 3 --max_n 10
+python src/gen_sr_dimacs.py ${DIR_TRAIN} ${DIR_LOG} 100 --min_n 3 --max_n 10
 
 # validation
 DIR_VAL = "data/dimacs/validation/sr10"
 DIR_LOG = "log/gen_sr10_validation.log"
-rm -rf $DIR_VAL
-mkdir -p $DIR_VAL
+rm -rf ${DIR_VAL}
+mkdir -p ${DIR_VAL}
 
-python src/gen_sr_dimacs.py $DIR_VAL $DIR_LOG 10 --min_n 10 --max_n 10
+python src/gen_sr_dimacs.py ${DIR_VAL} ${DIR_LOG} 10 --min_n 10 --max_n 10
 
 
