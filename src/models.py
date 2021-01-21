e5eabc9747c5be6c18b2d8d7ebc441ca18f05ae4
lee-man <minli.amoy@gmail.com>
2021-01-21 15:48:13 +0800

    update/

--- src/models.py
+++ src/models.py
@@ -226,7 +226,7 @@ class DVAEncoder(nn.Module):
             H_vb = self._propagate_from(G, self.max_n-1, self.grue_backward, 
                                  H0=self._get_zero_hidden(len(G)), reverse=True)
         for _ in range(self.n_rounds):
-            H_vf = self._propagate_from(G, 0, self.grue_forward, H0=self.H_vf,
+            H_vf = self._propagate_from(G, 0, self.grue_forward, H0=H_vf,
                              reverse=False)
             if self.bidir:
                 H_vb = self._propagate_from(G, self.max_n-1, self.grue_backward, 
