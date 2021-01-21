983ad4766f9970c6f712f7df5a5237b667ff69b8
lee-man <minli.amoy@gmail.com>
2021-01-21 11:48:27 +0800

    update.

--- scripts/aigabc2igraph.sh
+++ scripts/aigabc2igraph.sh
@@ -1,10 +1,10 @@
 # train
-DIR_AIG_ABC="data/aig_abc/train/sr3to10"
-DIR_IGRAPG="data"
-DATASET_NAME="sr3to10_train"
+# DIR_AIG_ABC="data/aig_abc/train/sr3to10"
+# DIR_IGRAPG="data"
+# DATASET_NAME="sr3to10_train"
 
 
-python src/aig_parser.py ${DIR_AIG_ABC} ${DIR_IGRAPG} ${DATASET_NAME}
+# python src/aig_parser.py ${DIR_AIG_ABC} ${DIR_IGRAPG} ${DATASET_NAME}
 
 # validation
 DIR_AIG_ABC="data/aig_abc/validation/sr10"
