1a2453c4c4296e671b001f6c3ef6d93f79bbcd51
lee-man <minli.amoy@gmail.com>
2021-02-04 19:42:43 +0800

    update.

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ parser.add_argument('--data-type', default='AIG', choices=['AIG'],
 parser.add_argument('--data-name', default='sr10', help='graph dataset name')
 parser.add_argument('--nvt', type=int, default=4, help='number of different node types, \
                     4 for AIG setting')
-parser.add_argument('--save-interval', type=int, default=100, metavar='N',
+parser.add_argument('--save-interval', type=int, default=10, metavar='N',
                     help='how many epochs to wait each time to save model states')
 parser.add_argument('--check-inteval', default=None, type=int, help='The intevral to check model weights.')
 parser.add_argument('--only-test', action='store_true', default=False,
