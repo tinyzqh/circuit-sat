26ff87ecb22e62182f497a10d889ad2b86ba4d92
lee-man <minli.amoy@gmail.com>
2021-03-05 12:16:38 +0800

    update.

--- scripts/data_gen_t3to4_v5.sh
+++ scripts/data_gen_t3to4_v5.sh
@@ -16,43 +16,43 @@
 # python src/gen_sr_dimacs.py ${DIR_VAL} ${DIR_LOG} 1000 --min_n 5 --max_n 5
 
 
-# train
-DIR_DIMACS="data/dimacs/train/sr3to4"
-DIR_AIG="data/aig_raw/train/sr3to4"
-DIR_LOG="log/dimacs2aig_sr3to4_train.log"
-rm -rf ${DIR_AIG}
-mkdir -p ${DIR_AIG}
-
-python src/dimacs2aig.py ${DIR_DIMACS} ${DIR_AIG} ${DIR_LOG}
-
-# validation
-DIR_DIMACS="data/dimacs/validation/sr5"
-DIR_AIG="data/aig_raw/validation/sr5"
-DIR_LOG="log/dimacs2aig_sr5_validation.log"
-rm -rf ${DIR_AIG}
-mkdir -p ${DIR_AIG}
-
-python src/dimacs2aig.py ${DIR_DIMACS} ${DIR_AIG} ${DIR_LOG}
-
-
-
 # # train
-# DIR_AIG="data/aig_raw/train/sr3"
-# DIR_AIG_ABC="data/aig_abc/train/sr3"
-# DIR_LOG="log/aig2aig_sr3_train.log"
-# rm -rf ${DIR_AIG_ABC}
-# mkdir -p ${DIR_AIG_ABC}
+# DIR_DIMACS="data/dimacs/train/sr3to4"
+# DIR_AIG="data/aig_raw/train/sr3to4"
+# DIR_LOG="log/dimacs2aig_sr3to4_train.log"
+# rm -rf ${DIR_AIG}
+# mkdir -p ${DIR_AIG}
 
-# python src/aig2abcaig.py ${DIR_AIG} ${DIR_AIG_ABC} ${DIR_LOG}
+# python src/dimacs2aig.py ${DIR_DIMACS} ${DIR_AIG} ${DIR_LOG}
 
 # # validation
-# DIR_AIG="data/aig_raw/validation/sr3"
-# DIR_AIG_ABC="data/aig_abc/validation/sr3"
-# DIR_LOG="log/aig2aig_sr3_validation.log"
-# rm -rf ${DIR_AIG_ABC}
-# mkdir -p ${DIR_AIG_ABC}
+# DIR_DIMACS="data/dimacs/validation/sr5"
+# DIR_AIG="data/aig_raw/validation/sr5"
+# DIR_LOG="log/dimacs2aig_sr5_validation.log"
+# rm -rf ${DIR_AIG}
+# mkdir -p ${DIR_AIG}
 
-# python src/aig2abcaig.py ${DIR_AIG} ${DIR_AIG_ABC} ${DIR_LOG}
+# python src/dimacs2aig.py ${DIR_DIMACS} ${DIR_AIG} ${DIR_LOG}
+
+
+
+# train
+DIR_AIG="data/aig_raw/train/sr3to4"
+DIR_AIG_ABC="data/aig_abc/train/sr3to4"
+DIR_LOG="log/aig2aig_sr3to4_train.log"
+rm -rf ${DIR_AIG_ABC}
+mkdir -p ${DIR_AIG_ABC}
+
+python src/aig2abcaig.py ${DIR_AIG} ${DIR_AIG_ABC} ${DIR_LOG}
+
+# validation
+DIR_AIG="data/aig_raw/validation/sr5"
+DIR_AIG_ABC="data/aig_abc/validation/sr5"
+DIR_LOG="log/aig2aig_sr5_validation.log"
+rm -rf ${DIR_AIG_ABC}
+mkdir -p ${DIR_AIG_ABC}
+
+python src/aig2abcaig.py ${DIR_AIG} ${DIR_AIG_ABC} ${DIR_LOG}
 
 
 
