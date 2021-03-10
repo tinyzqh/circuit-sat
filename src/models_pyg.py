790cd7c2a5ae85bf5773e46ea613504bb819ea3d
lee-man <minli.amoy@gmail.com>
2021-03-10 20:02:17 +0800

    update.

--- src/models_pyg.py
+++ src/models_pyg.py
@@ -207,6 +207,7 @@ class DVAEncoder_PYG(nn.Module):
         for g in G:
             print(g.vs[g.x.shape[0] - 1])
             hg = g.vs[g.x.shape[0] - 1]['H_forward']
+            exit()
             if bidir:  # decoding never uses backward propagation
                 hg_b = g.vs[0]['H_backward']
                 hg = torch.cat([hg, hg_b], 1)
