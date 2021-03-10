6617985a5cc4f86ea0e120403a5a913f85a32828
lee-man <minli.amoy@gmail.com>
2021-03-10 19:04:11 +0800

    update.

--- src/models_pyg.py
+++ src/models_pyg.py
@@ -222,7 +222,7 @@ class DVAEncoder_PYG(nn.Module):
         HLiteral = []
         solutions = []
         for g in G:
-            if g.solution:
+            if g.solution is not None:
                 for idx_literal in range(g.num_literals):
                     HLiteral.append(g.vs[idx_literal+1])['H_backward']
                     solutions.append([g.solution[idx_literal]])
