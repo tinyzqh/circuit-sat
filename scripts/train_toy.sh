ee4be673038d8dc369004f0740f11f524fa3d0aa
lee-man <minli.amoy@gmail.com>
2021-03-29 18:47:55 +0800

    update.

--- scripts/train_toy.sh
+++ scripts/train_toy.sh
@@ -1,14 +1,4 @@
 # The training script for tiny dataset: SR3 actually.
 python src/train.py --data-name  AIG  \
-                --model DVAEncoder \
-                --data-name sr3 \
-                --nvt 4 \
-                --save-interval 100 \
-                --hs 512 \
-                --nz 64 \
-                --bidirectional \
-                --lr 1e-3 \
-                --epochs 50 \
-                --n_rounds 10 \
-                --batch-size 128 
+                --model DGDAGRNN
 
